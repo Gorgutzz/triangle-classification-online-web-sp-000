517fc560d843ea79099dc9143ec032c213ff7e90
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-18 01:10:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,8 +19,8 @@ class Triangle
         :isosceles
     elsif (side1 != side2) && (side2 != side3) && (side1 != side3)
         :scalene
-      end
     end
+  end
 
 class TriangleError < StandardError
 
