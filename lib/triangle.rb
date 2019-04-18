935ba5301508180fcbe60c146a0fffbb17f9fc77
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-18 01:01:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,9 @@ class Triangle
       raise TriangleError
     elsif (side1 <= 0) || side2 <= 0 || side3 <= 0)
       raise TriangleError
-    elsif side1 == side2 || side2 == side3
+    elsif (side1 == side2) && (side2 == side3)
+      :equilateral
+    end
 
 
   def kind
