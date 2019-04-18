120251d2e2183bdc41cf6a39c2d49acb4a699742
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-18 00:47:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,16 @@
 class Triangle
   # write code here
-  def initialization()
+  attr_accessor :side1, :side2, :side3
 
+  def initialization(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
 
   def kind
-  end
 
+  end
 
     class TriangleError < StandardError
 
